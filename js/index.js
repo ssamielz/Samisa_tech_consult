c0ed2157a3a8b4b6d602832f29614afe53913be5
Sam <ssamsanya3@gmail.com>
2025-03-02 23:51:28 +0300

    js inserted

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let x; //assignment expression
+x = 50;
+console.log(x);
+
+let y = 20 - 3;
+console.log(y);
+
+let k = x - y;  //variable lookup expression
+console.log(k);
+
+let price = 2000;
+console.log(`The price is ${price}` );  //assignment expression printing in a sentence
+
+
+const className = "Titans" //constant expression declaration
+console.log(className)
+
+
+//declaring variables using var that allows redeclaration and reassign
+
+var boy = "Dennis"
+var boy = "Brian"
+console.log(boy);
+
+//using let declaration that allows reassigning but not redeclaration
+
+let balance = 20;
+balance = 30;
+console.log(balance);
